5a49ec42be46587af8a9ce091b9afb536014a213
lansongying-whut <littlelan.work@qq.com>
2016-02-10 17:24:38 +0800

    添加电场拉伸效果

--- rw_cubic_lattice_3d.py
+++ rw_cubic_lattice_3d.py
@@ -1,17 +1,39 @@
+# coding:utf-8
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
+import random
 
-chain_num = 10
-monomer_num = 2000
+chain_num = 1
+monomer_num = 10000
 bond_len = 0.1
+
+# 拉力
+fz = float(raw_input('请输入fz的值: '))
+# z轴方向正负概率
+Pzplus = np.exp(fz)/(np.cosh(fz)+2)/2
+Pzminus = np.exp(-fz)/(np.cosh(fz)+2)/2
+# 横向之间概率相同
+Pother = 1/(np.cosh(fz)+2)/2
+some_list = [0, 1, 2, 3, 4, 5]
+probabilities = [Pother, Pother, Pother, Pother, Pzplus, Pzminus]
+
+# 以指定概率取值
+def random_pick(some_list, probabilities):
+    x = random.uniform(0,1) # 返回(0, 1)之间的浮点数
+    cumulative_probability = 0.0
+    for item, item_probability in zip(some_list, probabilities):
+        cumulative_probability += item_probability
+        if x < cumulative_probability:break
+    return item
 
 def gen_line(monomer_num, dims=2):
     line_data = np.zeros((dims, monomer_num))
     line_data[:, 0] = [0, 0, 0]
     for i in range(1, monomer_num):
-        dir = np.random.randint(0,6)
+        dir = random_pick(some_list, probabilities)
+        # dir = np.random.randint(0,6)
         if dir == 0:
             step = [bond_len, 0, 0]
         if dir == 1:
@@ -41,5 +63,8 @@ lines = [ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in data]
 ax.set_xlim3d([0.0, 1.0])
 ax.set_ylim3d([0.0, 1.0])
 ax.set_zlim3d([0.0, 1.0])
-line_ani = animation.FuncAnimation(fig, update_line, monomer_num, fargs=(data, lines), interval=30, blit=False)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+line_ani = animation.FuncAnimation(fig, update_line, monomer_num, fargs=(data, lines), interval=10, repeat=False, blit=False)
 plt.show()
--- rwfigure.py
+++ rwfigure.py
@@ -10,6 +10,9 @@ const = np.linspace(1., 1., 50)
 gaussian = fz/3
 rw = np.sinh(fz)/(np.cosh(fz)+2)
 langevin = ((np.cosh(fz))/(np.sinh(fz))-1/fz)
+
+# 拉力和伸长关系
+plt.figure(1)
 line1=plt.plot(fz, gaussian, 'bo', label='Gaussian Chain')
 line2=plt.plot(fz, rw, 'ro', label='RW Chain')
 line3=plt.plot(fz, langevin, 'go', label='Langevin Chain')
@@ -18,5 +21,25 @@ plt.ylim(0.0, 1.5)
 plt.xlabel('Stretching force, '+r'$f_z$')
 plt.ylabel('Relative elongation, '+r'$R_z/R_{max}$')
 plt.title("Force-elongation Relation of a Freely-jointed Chain")
-legend = plt.legend(bbox_to_anchor=(1,0.25))
+legend = plt.legend(bbox_to_anchor=(1,0.3))
+
+# 拉力和方向概率关系
+zplus = np.exp(fz)/(2*np.cosh(fz)+4)
+zminu = np.exp(-fz)/(2*np.cosh(fz)+4)
+pplus = 1/(2*np.cosh(fz)+4)
+const2 = np.linspace(1./6., 1./6., 50)
+
+plt.figure(2)
+linep = plt.plot(fz, zplus, 'ro', label=r"$+z$")
+linem = plt.plot(fz, zminu, 'bo', label=r"$-z$")
+linepp = plt.plot(fz, pplus, 'g--', label=r"$+\rho$")
+linepm = plt.plot(fz, pplus, 'r-', label=r"$-\rho$")
+lineconst2 = plt.plot(fz, const2, '--', color='#455667')
+
+plt.xlim(-0.1, 5.0)
+plt.ylim(-0.1, 1.0)
+plt.xlabel('Stretching force, '+r'$f_z$')
+plt.ylabel('Stepping probabilities')
+plt.title('Stepping probabilities along the z axis')
+legend = plt.legend(bbox_to_anchor=(1,0.7))
 plt.show()
